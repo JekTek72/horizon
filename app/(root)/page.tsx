beaa3da59d28c2d5f44e158e1370e53813bb1a3a
JekTek <juanvicentebio653@gmail.com>
2024-08-19 16:24:51 +0200

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,24 @@
+import HeaderBox from '@/components/HeaderBox';
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firstName: 'Juan', lastName: 'Biosca', email: 'contacto@jektek.com' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox type="greeting" title="Bienvenido/a" user={loggedIn?.firstName || 'Invitado/a'} subtext="Accede y maneja tu cuenta y transacciones de forma efectiva" />
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+
+        TRANSACCIONES RECIENTES
+      </div>
+
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]} />
+    </section>
   )
 }
 
